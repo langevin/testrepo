1eb0162ebeb4d0554d477a1e7a09dfe51825aede
Christian D Langevin <langevin@usgs.gov>
2015-10-25 18:58:12 -0400

    still trying

--- py.test/t003_test.py
+++ py.test/t003_test.py
@@ -2,7 +2,7 @@ import subprocess
 
 def test_three():
     print('this is a test')
-    val = subprocess.call(['gfortran', '--version'])
+    val = subprocess.call(['gfortran'])
     raise Exception('Trying fortran compiler')
     return
     
